1286f9c2c25867822d67a109dc3c70ce3179a1dc
Grace Turkington <graceturkington97@gmail.com>
2020-05-19 17:22:21 +0100

    Validation added to textboxes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 import boto3
 import json
 
+import re
+
 from app import app
 
 from flask import jsonify
@@ -13,12 +15,40 @@ def index():
 
 @app.route("/sentiment", methods=["POST"])
 def sentiment():
-    text = request.form.get("input", 0, type=str)
-    comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
-    return jsonify(result=json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+    text = request.form.get("inputSentiment", 0, type=str)
+
+    if (text == ""):
+    	#No text was entered
+    	return(jsonify(result="You must enter text to be analysed for its sentiment."))
+    if (bool(re.search('[a-zA-Z]', text)) == False):
+        #Characters a-z were not found in the text
+        return(jsonify(result="You must enter text to be analysed for its sentiment, not only symbols."))
+    else:
+    	comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
+    	return jsonify(result=json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
 
 @app.route("/entity", methods=["POST"])
 def entity():
-    text = request.form.get("input2", 0, type=str)
-    comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
-    return jsonify(result=json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+    text = request.form.get("inputEntity", 0, type=str)
+    if (text == ""):
+    	#No text was entered
+    	return(jsonify(result="You must enter text to be analysed for its entities."))
+    if (bool(re.search('[a-zA-Z]', text)) == False):
+        #Characters a-z were not found in the text
+        return(jsonify(result="You must enter text to be analysed for its entities, not only symbols."))
+    else:
+    	comprehend = boto3.client(service_name='comprehend', region_name='us-west-2')
+    	return jsonify(result=json.dumps(comprehend.detect_entities(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+
+@app.route("/personality", methods=["POST"])
+def personality():
+    text = request.form.get("inputPersonality", 0, type=str)
+    if (text == ""):
+    	#No text was entered
+    	return(jsonify(result="You must enter text to be analysed for its personality."))
+    if (bool(re.search('[a-zA-Z]', text)) == False):
+        #Characters a-z were not found in the text
+        return(jsonify(result="You must enter text to be analysed for its personality, not only symbols."))
+    else:
+    	#ENTER
+    	return #ENTER
